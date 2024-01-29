ead32d3995d765f8282b440ea287fa1c596a4850
Saifullah772 <saifullahshah772@gmail.com>
2024-01-29 14:14:57 +0600

    Largest number

--- src/Largest.java
+++ src/Largest.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
 public class Largest {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+
+        //Q: Find the largest of 3 number
+
+        int max = a;
+        if (b>max){
+            max=b;
+        }
+        if (c>max){
+            max=c;
+        }
+        System.out.println(max);
+    }
+
 }
