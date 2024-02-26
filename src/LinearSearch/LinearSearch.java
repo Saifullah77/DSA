e45a49f8c684413a482b0a687772878fee9d9ba3
Saifullah772 <saifullahshah772@gmail.com>
2024-02-26 16:10:37 +0600

    LinearSearch.java

---
+++ src/LinearSearch/LinearSearch.java
@@ -0,0 +1,27 @@
+package LinearSearch;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int target = 9;
+        System.out.println(linearSearch(arr, target));
+    }
+    // search in the array: return the index if item found
+    // otherwise if item not found return -1
+    static int linearSearch(int[] arr, int target) {
+        if (arr.length == 0) {
+            return -1;
+        }
+        // run a for loop
+        for (int index = 0; index < arr.length; index++) {
+            // check for element at every index if it is = target
+            if (arr[index] == target) {
+                return index;
+            }
+        }
+        // this line will execute if none of the return statements above have executed
+        // hence the target not found
+        return -1;
+    }
+}
